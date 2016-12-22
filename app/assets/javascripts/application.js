6ca38a5fadffe02fb8e10145453b198f1922bea4
agrim mittal <agrimmittal97@gmail.com>
2016-12-23 00:13:09 +0530

    readded turbolinks for faster loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,22 +19,22 @@
 //= require select2
 //= require chat
 // require jquery.remotipart
-// require turbolinks
+//= require turbolinks
 //= require faye
 //= require_tree .
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
   $('.dropdown-button').dropdown({
-      inDuration: 300,
-      outDuration: 225,
+    inDuration: 300,
+    outDuration: 225,
       constrain_width: false, // Does not change width of dropdown to that of the activator
       hover: false, // Activate on hover
       gutter: 0, // Spacing from edge
       belowOrigin: true, // Displays dropdown below the button
       alignment: 'left' // Displays dropdown with edge aligned to the left of button
     }
-  );
-      $('.modal-trigger').leanModal();
-    
+    );
+  $('.modal-trigger').leanModal();
+  
   // Show sideNav
   $('.button-collapse').click(function(){
     $(".side-nav").show();
